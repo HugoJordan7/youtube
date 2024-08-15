cb0a49a8d3596461c14330eb90aa20c4bc7e4b3d
Hugo Jordan <hugoj8366@gmail.com>
2024-08-15 11:07:32 -0300

    feat: create main rv layout

--- app/src/main/java/com/example/youtube/feature/main/view/MainActivity.kt
+++ app/src/main/java/com/example/youtube/feature/main/view/MainActivity.kt
@@ -1,20 +1,36 @@
-package com.example.youtube
+package com.example.youtube.feature.main.view
 
+import android.graphics.drawable.GradientDrawable.Orientation
+import android.icu.lang.UCharacter.VerticalOrientation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.youtube.R
+import com.example.youtube.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.apply {
+            mainRvVideo.adapter = MainAdapter(emptyList())
+            mainRvVideo.layoutManager = LinearLayoutManager(this@MainActivity)
         }
     }
 }
