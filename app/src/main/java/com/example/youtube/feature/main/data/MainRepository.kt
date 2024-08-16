13d08caa86d6f0be9f0957afdd874ef88bdbc3ab
Hugo Jordan <hugoj8366@gmail.com>
2024-08-16 12:35:13 -0300

    refactor: reduce code in main repository

--- app/src/main/java/com/example/youtube/feature/main/data/MainRepository.kt
+++ app/src/main/java/com/example/youtube/feature/main/data/MainRepository.kt
@@ -6,17 +6,7 @@ import com.example.youtube.model.ListVideo
 class MainRepository(private val dataSource: MainDataSource) {
 
     suspend fun findVideoList(callback: RequestCallback<ListVideo>){
-        dataSource.findVideoList(object : RequestCallback<ListVideo>{
-            override fun onSuccess(data: ListVideo) {
-                callback.onSuccess(data)
-            }
-            override fun onFailure(message: String) {
-                callback.onFailure(message)
-            }
-            override fun onComplete() {
-                callback.onComplete()
-            }
-        })
+        dataSource.findVideoList(callback)
     }
 
 }
